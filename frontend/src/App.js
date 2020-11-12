4de23352e66d11bae5453a437bdf3424dd5f55f9
b.lusztak <b.lusztak@student.mini.pw.edu.pl>
2020-11-12 21:58:15 +0100

    correction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,11 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-<<<<<<< HEAD
-          Learn React asdasdads
-=======
           Learn React 
->>>>>>> develop
         </a>
       </header>
     </div>
