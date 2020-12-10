311548e7a18e5add17d871a8f16b51c0cd90887e
Vladyslav Yatsenko <jacenko.vlad@gmail.com>
2020-12-10 23:46:56 +0100

    TimelineWithServerInit

---
+++ backend/BackEnd/DataTransferObjects/EventResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.DataTransferObjects
+{
+    public class EventResponse
+    {
+        public DateTime Data { get; set; }
+        public string Tytul { get; set; }
+        public string Tresc { get; set; }
+    }
+}
