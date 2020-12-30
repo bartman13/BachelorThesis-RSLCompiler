39c5431bec8eaac3a61bb5ace7fd93bb215de2cf
yivlad <52505649+yivlad@users.noreply.github.com>
2020-12-30 23:06:06 +0100

    Apphistory reworked initial commit (#19)

--- frontend/src/components/AppHistoryComponent.js
+++ frontend/src/components/AppHistoryComponent.js
@@ -1,25 +1,194 @@
-import React from "react";
+import React, { useEffect, useState, useContext } from "react";
 import AppTimeline from "./TimeLine";
-import { Button, Grid } from '@material-ui/core';
+import { Button, Grid, Paper, makeStyles, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import UserContext from '../contexts/UserContext';
+import SnackbarContext from '../contexts/SnackbarContext';
+import LoadingContext from '../contexts/LoadingContext';
+import axios from 'axios';
+import authHeader from '../shared/authheader';
+import apiURL from '../shared/apiURL';
+import fileDownload from 'js-file-download';
+import LinkMUI from '@material-ui/core/Link';
 
-function AppHistory({appid}){
-    return(
-        <div className="container">
-            <div className="text-center"><h1>Historia zgłoszenia </h1></div>
-            <AppTimeline appid={appid}/>
-            <Grid container>
-                <Grid item xs={12} align='center'>
-                    <Button
-                        variant="outlined"
-                        color="primary"
-                        component={Link}
-                        to={'/addnop/' + appid}>
-                            Dodaj nowy odczyn
-                    </Button>
-                </Grid>
-            </Grid>
+const useStyles = makeStyles((theme) => ({
+    paper : {
+        padding: 20,
+        margin: 20
+    }
+}));
+
+function FileListAttribute( { attr }){
+    const [files, setFiles] = useState([]);
+
+    const { user } = useContext(UserContext);
+    const { setSnackbar } = useContext(SnackbarContext);
+
+    useEffect(() => {
+        attr.wartosc.split(';').forEach(async (filename) => {
+            try{
+                const fileInfo = await axios.get(apiURL + 'fileInfo/' + filename, authHeader(user));
+                setFiles(fs => [...fs, {name: fileInfo.data, link: filename}]);
+            } catch(error) {
+                console.error(error);
+                setSnackbar({
+                    open: true,
+                    type: 'error',
+                    message: 'Błąd ładowania danych'
+                });
+            }
+        });
+    }, [setFiles, user, attr, setSnackbar]);
+
+    const handleFileDownload = async (file) => {
+        try{
+            const blob = await axios.get(apiURL + 'file/' + file.link, {
+                responseType: 'blob',
+                ...authHeader(user)
+            });
+            fileDownload(blob.data, file.name);
+        } catch(error) {
+            console.error(error);
+            setSnackbar({
+                open: true,
+                type: 'error',
+                message: 'Nie udało się pobrać pliku'
+            });
+        }
+    };
+
+    const FileLink = ( { file } ) => {
+        return (
+            <LinkMUI
+                component="button"
+                variant="body2"
+                onClick={() => { handleFileDownload(file); }}
+            >
+                {file.name}
+            </LinkMUI>
+        );
+    };
+
+    return (
+        <div> Dodane pliki: <ol>{files.map((f, i) => <li key={i} > <FileLink file={f}/> </li>)} </ol> </div>
+    )
+}
+
+function EventDescription({ event }){
+    if(event.typ === 0){
+        return (
+            <div> {event.tresc} </div>
+        );
+    }
+    if(event.typ === 1){
+        return (
+            <div> {event.tytul} </div>
+        );
+    }
+    if(event.typ < 10 && event.typ > 4){
+        return (
+            <div>
+                Nowa decyzja lekarza: {event.tytul} ({event.tresc})
+            </div>
+        );
+    }
+    return (
+        <div>
+            Dodanie nowego odczynu - {event.tytul}:
+            <ul>
+                {event.atrybuty.map((a, i) => {
+                    if(a.typ === 2){
+                        return <li key={i}> <FileListAttribute attr={a}/> </li>
+                    }
+                    return <li key={i}> {a.nazwa} : {a.wartosc} </li>
+                })}
+            </ul>
         </div>
+    );
+}
+
+function ItemDescription({ item }){
+    if(!item){
+        return (
+            <Typography variant='h6' align='center'> Nie wybrano żadnego zdarzenia </Typography>
+        );
+    }
+    return (
+        <div>
+            <Typography variant='h6' align='center'> Dane o zdarzeniu </Typography>
+            <ol>
+                {item.zdarzenia.map((z, i) => {
+                    return (
+                        <li key={i}>
+                            <EventDescription event={z}/>
+                        </li>
+                    );
+                })}
+            </ol>
+        </div>
+    );
+}
+
+function AppHistory({ appid }){
+    const [timelineEvents, setTimelineEvents] = useState([]);
+    const [selectedEvent, setSelectedEvent] = useState(undefined);
+
+    const { user } = useContext(UserContext);
+    const { setLoading } = useContext(LoadingContext);
+    const { setSnackbar } = useContext(SnackbarContext);
+
+    const classes = useStyles();
+
+    useEffect(() => {
+        async function fetchData() {
+            setLoading(true);
+            try {
+                const eventsData = await axios.get(apiURL + "AppHistory/" + appid, authHeader(user));
+                setTimelineEvents(eventsData.data);
+                console.log(eventsData.data);
+            } catch (error) {
+                console.error(error);
+                setSnackbar({
+                    open: true,
+                    message: "Błąd ładowania danych",
+                    type: "error"
+                });
+            }
+            setLoading(false);
+        }
+
+        fetchData();
+    }, [appid, user, setLoading, setSnackbar]);
+
+    return(
+        <Grid container>
+            <Grid item md>
+                <Paper className={classes.paper}>
+                    <Grid container>
+                        <Grid item xs={12} align='center'>
+                            <Typography variant='h4'> Historia zgłoszenia </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <AppTimeline timelineEvents={timelineEvents} onItemClick={(item) => setSelectedEvent(item)}/>
+                        </Grid>
+                        <Grid item xs={12} align='center'>
+                            <Button
+                                variant="outlined"
+                                color="primary"
+                                component={Link}
+                                to={'/addnop/' + appid}>
+                                    Dodaj nowy odczyn
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Grid>
+            <Grid item md>
+                <Paper className={classes.paper}>
+                    <ItemDescription item={selectedEvent}/>
+                </Paper>
+            </Grid>
+        </Grid>
     );
 }
 
--- frontend/src/components/TimeLine.js
+++ frontend/src/components/TimeLine.js
@@ -1,11 +1,5 @@
-import React, { useEffect, useState, useContext } from 'react';
-import UserContext from '../contexts/UserContext';
-import SnackbarContext from '../contexts/SnackbarContext';
-import LoadingContext from '../contexts/LoadingContext';
+import React from 'react';
 import Timeline from '@material-ui/lab/Timeline';
-import axios from 'axios';
-import authHeader from '../shared/authheader';
-import apiURL from '../shared/apiURL';
 import TimelineItem from '@material-ui/lab/TimelineItem';
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
 import TimelineConnector from '@material-ui/lab/TimelineConnector';
@@ -13,143 +7,73 @@ import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
 import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
 import Typography from '@material-ui/core/Typography';
-import Popover from '@material-ui/core/Popover';
-import { makeStyles } from '@material-ui/core';
+import Tooltip from '@material-ui/core/Tooltip';
 
-const useStyles = makeStyles((theme) => ({
-  popover: {
-    pointerEvents: 'none',
-  },
-  paper: {
-    padding: theme.spacing(1),
-  },
-}));
-
-function AppTimelineDot({ type }){
-  switch (type){
-    case 0: return <TimelineDot variant="outlined" style={{borderColor: 'blue'}}/>;
-    case 1: return <TimelineDot variant="outlined" style={{borderColor: 'cyan'}}/>;
-    case 2: return <TimelineDot variant="outlined" style={{borderColor: 'green'}}/>;
-    case 3: return <TimelineDot variant="outlined" style={{borderColor: 'orange'}}/>;
-    case 4: return <TimelineDot variant="outlined" style={{borderColor: 'red'}}/>;
-    case 5: return <TimelineDot/>;
-    case 6: return <TimelineDot style={{borderColor: 'blue', backgroundColor: 'blue'}}/>;
-    case 7: return <TimelineDot style={{borderColor: 'green', backgroundColor: 'green'}}/>;
-    case 8: return <TimelineDot style={{borderColor: 'orange', backgroundColor: 'orange'}}/>;
-    default: return <TimelineDot style={{borderColor: 'red', backgroundColor: 'red'}}/>;
-  }
-}
-
-function AppTimelineItem({ item }){
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handlePopoverOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handlePopoverClose = () => {
-    setAnchorEl(null);
-  };
-
-  const FormatDate = (withTime, date) => {
-    if(withTime){
-      return new Date(date).toLocaleDateString("pl", { 
-        weekday: 'long', 
-        year: 'numeric', 
-        month: 'long',
-        day: 'numeric'
-      });
+function AppTimelineDot({ type }) {
+    switch (type) {
+        case 0: return <TimelineDot variant="outlined" style={{ borderColor: 'blue' }} />;
+        case 1: return <TimelineDot variant="outlined" style={{ borderColor: 'cyan' }} />;
+        case 2: return <TimelineDot variant="outlined" style={{ borderColor: 'green' }} />;
+        case 3: return <TimelineDot variant="outlined" style={{ borderColor: 'orange' }} />;
+        case 4: return <TimelineDot variant="outlined" style={{ borderColor: 'red' }} />;
+        case 5: return <TimelineDot />;
+        case 6: return <TimelineDot style={{ borderColor: 'blue', backgroundColor: 'blue' }} />;
+        case 7: return <TimelineDot style={{ borderColor: 'green', backgroundColor: 'green' }} />;
+        case 8: return <TimelineDot style={{ borderColor: 'orange', backgroundColor: 'orange' }} />;
+        default: return <TimelineDot style={{ borderColor: 'red', backgroundColor: 'red' }} />;
     }
-    return new Date(date).toLocaleDateString("pl", { 
-      weekday: 'long', 
-      year: 'numeric', 
-      month: 'long', 
-      day: 'numeric', 
-      hour : "2-digit", 
-      minute : "2-digit" 
-    });
-  }
-
-  const open = Boolean(anchorEl);
-
-  return (
-    <TimelineItem>
-      <TimelineOppositeContent>
-        <Typography color="textSecondary">
-          {FormatDate(item.typ === 0, item.data)} 
-        </Typography>
-      </TimelineOppositeContent>
-      <TimelineSeparator>
-        <AppTimelineDot type={item.typ} />
-        <TimelineConnector />
-      </TimelineSeparator>
-      <TimelineContent>
-        <Typography
-          aria-owns={open ? 'mouse-over-popover' : undefined}
-          aria-haspopup="true"
-          onMouseEnter={handlePopoverOpen}
-          onMouseLeave={handlePopoverClose}
-        > 
-          {item.tytul}
-        </Typography>
-        <Popover
-          id="mouse-over-popover"
-          className={classes.popover}
-          classes={{
-            paper: classes.paper,
-          }}
-          open={open}
-          anchorEl={anchorEl}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-          }}
-          onClose={handlePopoverClose}
-          disableRestoreFocus
-        >
-        <Typography> {item.zdarzenia.map((z, i) => <div key={i}> {(i + 1) + ". " + z.tytul + " - " + z.tresc } </div>)}</Typography>
-      </Popover>
-      </TimelineContent>
-    </TimelineItem>
-  );
 }
 
-export default function AppTimeline({ appid }) {
-  const [timelineEvents, setTimelineEvents] = useState([]);
+function AppTimelineItem({ item, onItemClick }) {
 
-  const { user } = useContext(UserContext);
-  const { setLoading } = useContext(LoadingContext);
-  const { setSnackbar } = useContext(SnackbarContext);
-
-  useEffect(() => {
-    async function fetchData(){
-      setLoading(true);
-      try{
-        const eventsData = await axios.get(apiURL + "AppHistory/" + appid, authHeader(user));
-        setTimelineEvents(eventsData.data);
-      } catch(error) {
-        console.error(error);
-        setSnackbar({
-            open: true,
-            message: "Błąd ładowania danych",
-            type: "error"
+    const FormatDate = (withTime, date) => {
+        if (withTime) {
+            return new Date(date).toLocaleDateString("pl", {
+                weekday: 'long',
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            });
+        }
+        return new Date(date).toLocaleDateString("pl", {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: "2-digit",
+            minute: "2-digit"
         });
-      }
-      setLoading(false);
     }
-    fetchData();
-  }, [appid, user, setLoading, setSnackbar]);
 
-  return (
-    <Timeline align="right">
-      {timelineEvents.map((item, index) => {
-        return <AppTimelineItem item={item} key={index}/>;
-      })}
-    </Timeline>
-  );
+    return (
+        <TimelineItem>
+            <TimelineOppositeContent>
+                <Typography color="textSecondary">
+                    {FormatDate(false, item.data)}
+                </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+                <AppTimelineDot type={item.typ} />
+                <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+                <Tooltip title="Kliknij żeby wybrać" placement="bottom-end">
+                    <Typography onClick={() => onItemClick(item)}>
+                        {item.tytul}
+                    </Typography>
+                </Tooltip>
+            </TimelineContent>
+        </TimelineItem>
+    );
+}
+
+export default function AppTimeline({ timelineEvents, onItemClick }) {
+
+    return (
+        <Timeline align="right">
+            {timelineEvents.map((item, index) => {
+                return <AppTimelineItem item={item} key={index} onItemClick={onItemClick}/>;
+            })}
+        </Timeline>
+    );
 }
