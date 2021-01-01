98d76ef65a5f47b71bd1e7785e82ea1ab5ee6ed3
bartman13 <37373422+bartman13@users.noreply.github.com>
2021-01-01 19:33:06 +0100

    Doctor Screen Added (#20)
    Co-authored-by: b.lusztak <b.lusztak@student.mini.pw.edu.pl>

--- backend/BackEnd/Controllers/LekarzController.cs
+++ backend/BackEnd/Controllers/LekarzController.cs
@@ -2,14 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using BackEnd.DataTransferObjects;
+using BackEnd.Models;
+using BackEnd.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BackEnd.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Role.Lekarz)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public class LekarzController : BaseController
     {
+        private readonly NopContext _context;
+        private readonly IMapper _mapper;
+
+        public LekarzController(NopContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpGet("[controller]/Zgloszenia")]
+        public IActionResult GetApps()
+        {
+            var zgloszenia = (from Zgloszenia in _context.Zgloszenia
+                              .Include("Uzyt") 
+                              .Include("DecyzjeLekarza")// w przyszlosci zamienic na data transfer objet
+                       where Zgloszenia.LekarzId == Account.Id
+                       select Zgloszenia).ToList();
+
+            var ret = new List<DoctorAppResponse>();
+            foreach(var el in zgloszenia)
+            {
+                ret.Add(new DoctorAppResponse
+                {
+                    Id = el.Id,
+                    Data = el.Data,
+                    Nazwa_Szczepionki = "Polfarma",
+                    Imie = el.Uzyt.Imie,
+                    Nazwisko = el.Uzyt.Nazwisko,
+                    Status = el.DecyzjeLekarza.Count == 0 ? false : true
+
+                });
+            }
+            return Ok(ret);
+        }
     }
 }
