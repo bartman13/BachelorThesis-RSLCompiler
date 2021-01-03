227ff54e6e7f0c3c7ddedf07d0e22137532d76e5
Vladyslav Yatsenko <jacenko.vlad@gmail.com>
2021-01-04 00:06:12 +0100

    Parent sign up initial commit

--- frontend/src/components/SignInComponent.js
+++ frontend/src/components/SignInComponent.js
@@ -15,156 +15,160 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import apiURL from '../shared/apiURL';
 import LoadingContext from '../contexts/LoadingContext';
+import { useHistory } from 'react-router-dom';
 
-const authorize =  async (values) => {
-    try{
-      const response = await axios.post(
-        apiURL + 'signin', 
-        { email : values.email, haslo: values.password },
-        { withCredentials: true });
-      return response.data;
-    }catch(error){
-      console.error(error);
-    }
+const authorize = async (values) => {
+        try{
+            const response = await axios.post(
+                apiURL + 'signin', 
+                { email : values.email, haslo: values.password },
+                { withCredentials: true });
+            return response.data;
+        }catch(error){
+            console.error(error);
+        }
 }
 function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Bartosz Lusztak &amp; Vladyslav Yatsenko Inc.
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Bartosz Lusztak &amp; Vladyslav Yatsenko Inc.
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%',
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%',
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
 }));
 
 export default function SignIn(props) {
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [remember, setRemember] = useState(false);
-  const [emailValid, setEmailValid] = useState(true);
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    const [remember, setRemember] = useState(false);
+    const [emailValid, setEmailValid] = useState(true);
 
-  const { setLoading } = useContext(LoadingContext);
+    const { setLoading } = useContext(LoadingContext);
 
-  const { startRefreshToken } = props;
+    const { startRefreshToken } = props;
 
-  const classes = useStyles();
+    const classes = useStyles();
 
-  const handleSubmit = async (values) => {
-    setLoading(true);
-    const userData = await authorize(values);
-    if(userData && (userData.rola !== undefined)){
-      await startRefreshToken();
+    const history = useHistory();
+
+    const handleSubmit = async (values) => {
+        setLoading(true);
+        const userData = await authorize(values);
+        if(userData && (userData.rola !== undefined)){
+            await startRefreshToken();
+        }
+        setLoading(false);
     }
-    setLoading(false);
-  }
 
-  const emailChanged = (event) => {
-    setEmail(event.target.value);
-    if(/.+@.+\.[A-Za-z]+$/.test(event.target.value)){
-        setEmailValid(true);
-    }else{
-        setEmailValid(false);
+    const emailChanged = (event) => {
+        setEmail(event.target.value);
+        if(/.+@.+\.[A-Za-z]+$/.test(event.target.value)){
+            setEmailValid(true);
+        }else{
+            setEmailValid(false);
+        }
     }
-  }
+    console.log(props);
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Zaloguj się
-        </Typography>
-        <form className={classes.form} noValidate onSubmit={
-          (event) => {
-            event.preventDefault(); 
-            handleSubmit({email, password, remember});
-          }}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Adres Email"
-            name="email"
-            autoComplete="email"
-            autoFocus
-            onChange={(event)=>{emailChanged(event)}}
-            error={!emailValid}
-            helperText={!emailValid && "Niepoprawny adres email"}
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Hasło"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            onChange={(event)=>{setPassword(event.target.value)}}
-          />
-          <FormControlLabel
-            control={<Checkbox 
-                        value="remember"
-                        color="primary" 
-                        checked={remember}
-                        onChange={(event)=>{setRemember(event.target.checked)}}/>}
-            label="Zapamiętaj mnie"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Zaloguj
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Zapomniałeś hasła?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Nie masz konta? Utwórz je"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Copyright />
-      </Box>
-    </Container>
-  );
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Zaloguj się
+                </Typography>
+                <form className={classes.form} noValidate onSubmit={
+                    (event) => {
+                        event.preventDefault(); 
+                        handleSubmit({email, password, remember});
+                    }}>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Adres Email"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        onChange={(event)=>{emailChanged(event)}}
+                        error={!emailValid}
+                        helperText={!emailValid && "Niepoprawny adres email"}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Hasło"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        onChange={(event)=>{setPassword(event.target.value)}}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox 
+                                    value="remember"
+                                    color="primary" 
+                                    checked={remember}
+                                    onChange={(event)=>{setRemember(event.target.checked)}}/>}
+                        label="Zapamiętaj mnie"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Zaloguj
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="" variant="body2">
+                                Zapomniałeś hasła?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="" variant="body2" onClick={() => history.push('/signup')}>
+                                {"Nie masz konta? Utwórz je"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            <Box mt={8}>
+                <Copyright />
+            </Box>
+        </Container>
+    );
 }
